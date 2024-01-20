716f2bc0ec317388cc27ba5bde879b04cc84a9cc
swwayamm <codingswayam24@gmail.com>
2024-01-21 00:13:07 +0530

    Solution First

---
+++ script.js
@@ -0,0 +1,26 @@
+// Variables to select html elements
+
+const heading = document.getElementById("heading");
+const headingSize = document.getElementById("heading-size");
+
+const subHeading = document.getElementById("sub-heading");
+const subHeadingSize = document.getElementById("sub-heading-size");
+
+const btnSize = document.getElementById("btn-size");
+
+const iframe = document.getElementById("iframe");
+
+// Functions to perform operations 
+function setData() {}
+
+function changeHeading() {}
+
+function changeHeadingSize() {}
+
+function changeSubHeading() {}
+
+function changeSubHeadingSize() {}
+
+function changeBtnText() {}
+
+function changeBtnTextSize() {}
