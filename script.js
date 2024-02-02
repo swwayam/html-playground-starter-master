dea67fcd3d99579c4379e876069e6e573205d6a6
swwayamm <codingswayam24@gmail.com>
2024-02-02 18:45:57 +0530

    new update

---
+++ script.js
@@ -0,0 +1,32 @@
+// Fill in the variable values by looking at the id's of the input element in the index.html file
+
+// First one is already done for you
+let heading = document.getElementById("heading")
+let headingSize;
+
+let subHeading;
+let subHeadingSize;
+
+let btn;
+let btnSize;
+
+let iframe;
+let editableFrame;
+
+// Change code above this line
+
+iframe.onload = function () {};
+
+function setData() {}
+
+function changeHeading() {}
+
+function headingSizeChange() {}
+
+function changeSubHeading() {}
+
+function subHeadingSizeChange() {}
+
+function changeButtonText() {}
+
+function btnSizeChange() {}
